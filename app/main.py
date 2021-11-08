6aaf87835c6e273484e146c6edb8b844ed32ef06
Karan Yadav <ksyadav01@outlook.com>
2021-11-08 18:32:29 -0500

    updates

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, url_for
 from werkzeug.utils import redirect
+from riotwatcher import LolWatcher, ApiError
 import os
-from app import ingame_search
+
+# from app import ingame_search
+
 app = Flask(__name__)
 API_KEY = os.environ.get('API_KEY')
 
@@ -17,10 +20,25 @@ def home():
         global InitialList
         global InGameList
         global NotInGameList
+
         InitialList = request.form.get('NameList')
-        InGameList, NotInGameList = ingame_search.player_ingame(InitialList, API_KEY)
+        watcher = LolWatcher(key)
+        region = "na1"
+        nameList = InitialList.split(", ")
+        InGameList.append('Players In Game')
+        NotInGameList.append('Players Not In Game')
+
+        for name in nameList:
+            player = watcher.summoner.by_name(region, "name")
+            try:
+                playerInGame = watcher.spectator.by_summoner(region, player['id'])
+                InGameList.append(name)
+                
+            except:
+                NotInGameList.append(name)
+        # InGameList, NotInGameList = ingame_search.player_ingame(InitialList, API_KEY)
         return redirect(url_for('ingame_list'))
-    return "<h1>Welcome to CodingX</h1>" #render_template('ingame_search.html')
+    return "<h1>Welcome to CodingX</h1>"  # render_template('ingame_search.html')
 
 
 @app.route('/ingame_list')
